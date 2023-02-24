26c83fa6ea65cb346a576257e30afad8dee4ff76
anishbh <anishbhupalam@gmail.com>
2023-02-24 00:17:28 -0500

    finishing getspecs code

--- getspecs.py
+++ getspecs.py
@@ -1,23 +1,48 @@
-
+#TODO: find the actual spec number, not just the string 
+#TODO: find name of item (easier to do in getlinks) 
 import requests
 from bs4 import BeautifulSoup
 
-#from getlinks import urls
-
 class Product:
-    def __init__(self, name, url, specs):
-        self.name = name
-        self.url = url
-        self.specs = specs
+        def __init__(self, name, url, specs):
+            self.name = name
+            self.url = url
+            self.specs = specs
 
-products = []
-urls = ["https://satsearch.co/products/tensortech-cmg-40m-control-moment-gyroscope"]
-for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+def get_products(urls):
+    products = []
 
-    divs = soup.find('div', {'class': 'specs-table'}).find_all('div', string=True)
-    print(divs)
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        divs = soup.find('div', {'class': 'specs-table'}).find_all('div', {'class': 'border-bottom'})
+
+        specs = {}
+        for i in range(0, len(divs), 2):
+            specs[divs[i].get_text()] = divs[i+1].get_text() #fix
+        
+        products.append(Product(url, url, specs))
+    
+    return products
+    
+def main():
+    urls = ["https://satsearch.co/products/tensortech-cmg-40m-control-moment-gyroscope", 
+            "https://satsearch.co/products/tensortech-cmg-20m-control-moment-gyroscope",
+            "https://satsearch.co/products/berlin-space-tech-rwa05"]
+    
+    products = get_products(urls)
+
+    for p in products: 
+        print(p.name, p.specs)
+
+
+if __name__ == "__main__":
+    main()
+        
+        
+
+
 
 
 
